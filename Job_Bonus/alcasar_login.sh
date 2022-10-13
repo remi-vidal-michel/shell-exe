2428e3ebd5bfc4dd23010e1d2799419f8e8e2d24
remi-vidal-michel <remi.vidal-michel@laplateforme.io>
2022-10-13 18:42:30 +0200

    script connexion alcasar

--- Job_Bonus/alcasar_login.sh
+++ Job_Bonus/alcasar_login.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-chall=$(curl -i https://alcasar.laplateforme.io/intercept.php --location-trusted | grep -o -P '(?<=challenge=).*(?=&called)')
-
-curl \
-    -d "challenge=$chall&username=remi.vidal-michel@laplateforme.io&password=mwVHS8j2" \
-    -H "Content-Type: application/x-www-form-urlencoded" \
-    -X POST https://alcasar.laplateforme.io/intercept.php \
-    -L 
+chall=$(curl -i https://alcasar.laplateforme.io/intercept.php -L  | grep -o -P '(?<=challenge=).*(?=&called)')
+curl -H "Content-Type: multipart/form-data" -X POST -F challenge=$chall -F username=remi.vidal-michel@laplateforme.io -F password=mwVHS8j2 -F button=Authentication https://alcasar.laplateforme.io/intercept.php -L
