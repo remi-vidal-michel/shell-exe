b70887b5591e2b83059b7a44a85086b2e34b5a18
remi-vidal-michel <remi.vidal-michel@laplateforme.io>
2022-10-14 10:34:12 +0200

    script login alcasar anyuser + silent output + user friendly

--- Job_Bonus/alcasar_login.sh
+++ Job_Bonus/alcasar_login.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-chall=$(curl -i https://alcasar.laplateforme.io/intercept.php -L  | grep -o -P '(?<=challenge=).*(?=&called)')
-curl -H "Content-Type: multipart/form-data" -X POST -F challenge=$chall -F username=$1 -F password=$2 -F button=Authentication https://alcasar.laplateforme.io/intercept.php -L
+if [ -z "$1" ]
+then echo "Erreur : Vous devez fournir vos identifiants de connexion" ; exit ; fi
+chall=$(curl -i -s https://alcasar.laplateforme.io/intercept.php -L | grep -o -P '(?<=challenge=).*(?=&called)')
+if [ -z "$chall" ]
+then echo "Erreur : Vous êtes déjà connecté" ; exit ; fi
+curl -s -o /dev/null -H "Content-Type: multipart/form-data" -X POST -F challenge=$chall -F username=$1 -F password=$2 -F button=Authentication https://alcasar.laplateforme.io/intercept.php -L
+if ping -c1 google.com &> /dev/null
+then echo "Succès ! Vous êtes connectés" 
+else echo "Erreur de connexion : vos identifiants sont peut-être incorrects ?" ; fi
