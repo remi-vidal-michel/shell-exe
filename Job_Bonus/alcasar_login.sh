6e2789e6984c5cda0e5a5c55a99d2fe4397b15a0
remi-vidal-michel <remi.vidal-michel@laplateforme.io>
2022-10-14 09:53:15 +0200

    script login alcasar + variables anyuser

--- Job_Bonus/alcasar_login.sh
+++ Job_Bonus/alcasar_login.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 chall=$(curl -i https://alcasar.laplateforme.io/intercept.php -L  | grep -o -P '(?<=challenge=).*(?=&called)')
-curl -H "Content-Type: multipart/form-data" -X POST -F challenge=$chall -F username=remi.vidal-michel@laplateforme.io -F password=mwVHS8j2 -F button=Authentication https://alcasar.laplateforme.io/intercept.php -L
+curl -H "Content-Type: multipart/form-data" -X POST -F challenge=$chall -F username=$1 -F password=$2 -F button=Authentication https://alcasar.laplateforme.io/intercept.php -L
